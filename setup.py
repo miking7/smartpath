0b466d8e145c354b19f6596a4873b0d336755a82
Michael Engelbrecht <michaelndani@gmail.com>
2019-12-22 16:35:25 +1100

    0.14 Fix broken "executable" permissions for getsecs.php

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
 setup(name='smartpath',
-	version='0.12',
+	version='0.14',
+	python_requires='==2.7.*',
 	description='smartpath implements the SmartFileSystem class (and connector classes) to provide unified object-oriented access to filesystems of various protocols.  It draws a number of parallels to the Windows FileSystemObject',
 	url='http://github.com/miking7/smartpath',
 	author='Michael Engelbrecht',
